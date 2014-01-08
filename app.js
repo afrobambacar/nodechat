aefff6c25f17249563418319bd4707417c69d174
afrobambacar <jclee@soribada.com>
2014-01-08 12:16:38 +0900

    socket.io example

---
+++ app.js
@@ -0,0 +1,19 @@
+var http = require('http');
+var fs = require('fs');
+var socketio = require('socket.io');
+
+var server = http.createServer(function (request, response) {
+	fs.readFile('index.html', function (error, data) {
+		response.writeHead(200, { 'Content-Type': 'text/html' });
+		response.end(data);
+	});
+}).listen(52273, function () {
+	console.log('Run Run Run!');
+});
+
+var io = socketio.listen(server);
+io.sockets.on('connection', function (socket) {
+	socket.on('message', function (data) {
+		io.sockets.emit('message', data);
+	});
+});
